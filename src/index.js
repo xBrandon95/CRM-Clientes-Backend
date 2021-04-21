a99290631636290571a967f335ecc2fe9015e724
Brandon Ticona <jbticona2@gmail.com>
2021-04-21 14:26:20 -0400

    ajustes para el deploy

--- src/index.js
+++ src/index.js
@@ -9,11 +9,29 @@ dotenv.config();
 // crear el servidor
 const app = express();
 
+// carpeta publica
+app.use(express.static('src/uploads'));
+
 // habilitando json
 app.use(express.json());
 
+// Definir dominio para recibir las peticiones
+const whitelist = [process.env.FRONTEND_URL];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    // revisar la peticion
+    const existe = whitelist.some(dominio => dominio === origin);
+    if (existe) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido por cors'));
+    }
+  },
+};
+
 // habilitando colors
-app.use(cors());
+app.use(cors(corsOptions));
 
 // coneccion a la base de datos
 dbConnection();
@@ -21,18 +39,16 @@ dbConnection();
 // utilizando morgan
 app.use(morgan('dev'));
 
-// carpeta publica
-app.use(express.static('src/uploads'));
-
 // rutas de la app
 app.use('/api', require('./routes/usuarios'));
 app.use('/api/clientes', require('./routes/clientes'));
 app.use('/api/productos', require('./routes/productos'));
 app.use('/api/pedidos', require('./routes/pedidos'));
 
-// definiendo el puerto
+// definiendo el puerto y hoste
+const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => {
+app.listen(port, host, () => {
   console.log(`Servidor corriendo en el puerto ${port}`);
 });
